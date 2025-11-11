abcef237c4be45d95b605226d3c75f1a6e7dadff
Rk-Yaduvanshi <yadavrakesh070@gmail.com>
2025-11-11 19:06:56 +0530

    Update files

--- sticky-social.php
+++ sticky-social.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enhanced Contact System</title>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         /* ===========================
@@ -436,5 +431,3 @@
             });
         });
     </script>
-</body>
-</html>
